6f951b5d4458646e2975171cd10bf2e89506704c
unknown <tvot21@gmail.com>
2022-10-16 16:06:49 +0300

    task68

---
+++ task67/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+//найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число M");
+int numM = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N");
+int numN = int.Parse(Console.ReadLine()!);
+
+
+void ShowNumsBetween(int min, int max)
+{
+
+    if (min == max)
+    {
+        Console.Write(min);
+        return;
+    }
+    if (min > max)
+    {
+        Console.Write($"{min} ");
+        ShowNumsBetween(min - 1, max);
+    }
+    else
+    {
+        Console.Write($"{max} ");
+        ShowNumsBetween(min, max - 1);
+    }
+}
+
+ShowNumsBetween(numM, numN);
+Console.WriteLine();
+
+SumArray(numM, numN);
+
+
+void SumArray(int numM, int numN)
+{
+    if (numM == numN)
+    {
+        Console.Write(numM);
+        return;
+    }
+    if (numM < numN)
+    {
+        int sum = 0;
+
+        for (int i = 0; i <= numN - numM; i++)
+        {
+            sum = numM + i + sum;
+
+        }
+        Console.WriteLine($"Сумма элементов от М до N равна = {sum}");
+    }
+    else
+    {
+        int sum = 0;
+        for (int i = 0; i <= numM - numN; i++)
+        {
+            sum = numM - i + sum;
+
+        }
+        Console.WriteLine($"Сумма элементов от М до N равна = {sum}");
+    }
+}
---
+++ task68/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите число M");
+int numM = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N");
+int numN = int.Parse(Console.ReadLine()!);
+
+
+Akkerman(numM, numN);
+
+
+int Akkerman(int numM, int numN)
+{
+    if (numM == numN)
+    {
+        Console.Write(numM);
+        return numM;
+    }
+
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+
+
+    if (numM > 0 && numN == 0)
+    {
+        return Akkerman(numM - 1, 1);
+    }
+
+    if (numN == 0)
+    {
+        return Akkerman(numM - 1, 1);
+    }
+
+    if (numM > 0 && numN > 0)
+    {
+        return Akkerman(numM - 1, Akkerman(numM, numN - 1));
+
+    }
+    return Akkerman(numM, numN);
+}
+
+Console.WriteLine($"Значение функции Аккермана = {Akkerman(numM, numN)}");
+
+
+
